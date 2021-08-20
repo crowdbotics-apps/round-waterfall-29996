8b2ba398ad48db3c6e87f89460c22f2eb0fbec64
Crowdbotics <team@crowdbotics.com>
2021-08-20 23:32:36 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "round_waterfall_29996Identifier",
+  url: "https://round_waterfall_29996Identifier.botics.co",
+  api: "https://round_waterfall_29996Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
